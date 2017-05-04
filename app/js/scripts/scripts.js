9d4c1f0021a0f16737f4acd64ba4fc2359852b25
alexrewrew <alexrewrew@MacBook-Pro-Oleksandr.local>
2017-05-04 17:39:34 +0300

    Papiroska

--- app/js/scripts/scripts.js
+++ app/js/scripts/scripts.js
@@ -39,27 +39,12 @@ $(document).ready(function () {
     });
 
 
-    //
-    var inputs = document.querySelectorAll( '.inputfile' );
-    Array.prototype.forEach.call( inputs, function( input )
-    {
-        var label	 = input.nextElementSibling,
-            labelVal = label.innerHTML;
-
-        input.addEventListener( 'change', function( e )
-        {
-            var fileName = '';
-            if( this.files && this.files.length > 1 )
-                fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
-            else
-                fileName = e.target.value.split( '\\' ).pop();
-
-            if( fileName )
-                label.querySelector( 'span' ).innerHTML = fileName;
-            else
-                label.innerHTML = labelVal;
-        });
-    });
+    //link-organization
+    $('.link-organization').click(function(){
+        event.preventDefault();
+        $('.input-organization').toggleClass('visible');
+        // $('.input-organization').slideToggle(1000);
+    })
 
     //slider
     var slider = $(".slider").lightSlider({
